d9742c1543b41dd89de643d10b5b45ca29848a88
Camilo López <camiloj.lopez79@gmail.com>
2023-02-22 16:45:53 -0300

    Next - GetStaticProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,25 @@ import { Button } from '@nextui-org/react';
 // Layouts
 import { MainLayout } from '@/layouts';
 
-export default function Home() {
+export default function Home( props ) {
+  console.log({ props });
+
   return (
     <MainLayout title='Listado de Pokemons'>
       <Button
         color='gradient'
       >
-        Hola Mundo
+        { props.name }
       </Button>
     </MainLayout>
   );
+}
+
+export const getStaticProps = async (ctx) => {
+  console.log( 'Hola' );
+  return {
+    props: {
+      name: 'Camilo'
+    }
+  }
 }
