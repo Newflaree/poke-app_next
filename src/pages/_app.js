913edd7bd1acebd0200290d3adb28b9c0c0054ae
Camilo López <camiloj.lopez79@gmail.com>
2023-02-22 15:25:24 -0300

    Implementando el tema Dark

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import { NextUIProvider } from '@nextui-org/react';
 // Styles
 import '@/styles/globals.css';
+// Themes
+import { darkTheme } from '@/themes';
 
 export default function App({ Component, pageProps }) {
 
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={ darkTheme }>
       <Component {...pageProps} />
     </NextUIProvider>
   );
