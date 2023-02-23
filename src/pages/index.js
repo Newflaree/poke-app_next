f22dbb2849ec30245da4703619254ec221ad8c63
Camilo López <camiloj.lopez79@gmail.com>
2023-02-23 11:52:02 -0300

    Tarea - Mostrar el nombre y número del Pokemon

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,18 @@
 import {pokeApi} from '@/api';
 import { MainLayout } from '@/layouts';
 
-export default function Home( props ) {
-
+export default function Home({ pokemons }) {
+  console.log( pokemons );
   return (
     <MainLayout title='Listado de Pokemons'>
       <ul>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
+        {
+          pokemons.map( ({ id, name }) => (
+            <li key={ id }>
+              #{ id } - { name }
+            </li>
+          ))
+        }
       </ul>
     </MainLayout>
   );
@@ -19,9 +21,15 @@ export default function Home( props ) {
 
 export const getStaticProps = async (ctx) => {
   const { data } = await pokeApi.get('/pokemon?limit=151')
+  const pokemons = data.results.map( (poke, i) => ({
+    ...poke,
+    id: i + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ i + 1 }.svg`
+  }));
+
   return {
     props: {
-      pokemons: data.results
+      pokemons
     }
   }
 }
